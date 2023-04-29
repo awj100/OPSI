c311e5c2ebc3bd4a8f367edd8ad23d5b672bb4f9
Andrew Jameson <andrew_jameson@hotmail.com>
2023-04-29 10:14:15 +0200

    NuGet updates.

---
+++ Opsi.Services/Types/StorageModel/FolderModel.cs
@@ -0,0 +1,24 @@
+﻿namespace Opsi.Services.Types.StorageModel;
+
+public class FolderModel : BlobModel
+{
+    private const string _type = "Folder";
+
+    public FolderModel(string fullName, ICollection<BlobModel> items) : base(fullName, _type)
+    {
+        Items = items;
+    }
+
+    public ICollection<BlobModel> Items { get; }
+
+    protected override string? GetName()
+    {
+        const char pathSeparator = '/';
+        return FullName.TrimEnd(pathSeparator).Split(pathSeparator).LastOrDefault();
+    }
+
+    public override string ToString()
+    {
+        return $"{FullName} ({Items.Count} item{(Items.Count == 1 ? String.Empty : "s")})";
+    }
+}
