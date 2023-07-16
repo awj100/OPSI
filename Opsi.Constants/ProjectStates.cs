a1c5ddb1aec8d30bcc32064f2e4d5e25d0b5e80d
Andrew Jameson <andrew_jameson@hotmail.com>
2023-07-16 08:00:19 +0200

    Added ProjectState.Deleted.

--- Opsi.Constants/ProjectStates.cs
+++ Opsi.Constants/ProjectStates.cs
@@ -3,6 +3,7 @@
 public static class ProjectStates
 {
     public readonly static string Completed = nameof(Completed);
+    public readonly static string Deleted = nameof(Deleted);
     public readonly static string Error = nameof(Error);
     public readonly static string Initialising = nameof(Initialising);
     public readonly static string InProgress = nameof(InProgress);
