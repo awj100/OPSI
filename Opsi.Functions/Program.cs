4ec3b1e7ed487526643871c0939f68c9bc9bf100
Andrew Jameson <andrew_jameson@hotmail.com>
2023-07-20 19:40:26 +0200

    Configured JSON serialisation services to use camel-case throughout.

--- Opsi.Functions/Program.cs
+++ Opsi.Functions/Program.cs
@@ -1,4 +1,7 @@
-﻿using Functions.Worker.ContextAccessor;
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using Functions.Worker.ContextAccessor;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Opsi.AzureStorage.DiModules;
 using Opsi.ErrorReporting.Services.DiModules;
@@ -19,6 +22,12 @@ var host = new HostBuilder()
     .ConfigureServices(services =>
     {
         services.AddFunctionContextAccessor();
+
+        services.Configure<JsonSerializerOptions>(options =>
+        {
+            options.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+            options.Converters.Add(new JsonStringEnumConverter());
+        });
 
         AzureStorageDiModule.Configure(services);
         ErrorServicesDiModule.Configure(services);
