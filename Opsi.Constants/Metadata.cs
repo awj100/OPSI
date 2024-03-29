5818daec91eb92bffc475b4628ebe4a3c4be40b1
Andrew Jameson <andrew_jameson@hotmail.com>
2024-03-29 09:38:01 +0100

    New constants for tagging and metadata on new .tags blob.

---
+++ Opsi.Constants/Metadata.cs
@@ -0,0 +1,8 @@
+namespace Opsi.Constants;
+
+public static class Metadata
+{
+    public static readonly string CreatedBy = nameof(CreatedBy);
+    public static readonly string ProjectId = nameof(ProjectId);
+    public static readonly string ProjectName = nameof(ProjectName);
+}
---
+++ Opsi.Constants/Tags.cs
@@ -0,0 +1,14 @@
+﻿namespace Opsi.Constants;
+
+public static class Tags
+{
+    public const string Assignee = nameof(Assignee);
+
+    public const string ProjectId = nameof(ProjectId);
+
+    public const string ProjectName = nameof(ProjectName);
+
+    public const string ProjectState = nameof(ProjectState);
+
+    public const string TagsHostName = ".tags";
+}
